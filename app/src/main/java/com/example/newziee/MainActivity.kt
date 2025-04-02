0347b07475a5583fb0d6d985484d807b21ba6daa
Odiljon <afewminutes777@gmail.com>
2025-04-02 11:10:05 +0500

    started

---
+++ app/src/main/java/com/example/newziee/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.newziee
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.newziee.ui.theme.NewzieeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NewzieeTheme {
+
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NewzieeTheme {
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreviewDark() {
+    NewzieeTheme(darkTheme = true) {
+
+    }
+}
---
+++ app/src/main/java/com/example/newziee/ui/theme/Color.kt
@@ -0,0 +1,1 @@
+package com.example.newziee.ui.theme
